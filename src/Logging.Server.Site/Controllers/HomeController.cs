ece1b3d831c30599929c8d975cde3a258858f52a
xumingxiang <115402708@qq.com>
2017-02-12 16:50:36 +0800

    【del】删除无效代码

--- src/Logging.Server.Site/Controllers/HomeController.cs
+++ src/Logging.Server.Site/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Logging.Server.Site.Controllers
 {
@@ -10,26 +6,7 @@ namespace Logging.Server.Site.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            return Content("this is netcorelogging server !");
         }
     }
 }
